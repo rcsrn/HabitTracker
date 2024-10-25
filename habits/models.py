a3196bba9f8dd601d80f96f77ba2ccc935ba7f40
casarin <nsgbanks@ciencias.unam.mx>
2024-10-24 22:34:43 -0600

    added models

--- habits/models.py
+++ habits/models.py
@@ -1,25 +1,26 @@
-
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# Obtener el modelo de usuario personalizado
+Usuario = get_user_model()
 
 class Habit(models.Model):
-    DAILY = 'Daily'
-    WEEKLY = 'Weekly'
-    MONTHLY = 'Monthly'
-    
+    DIARIO = 'Diario'
+    SEMANAL = 'Semanal'
+    MENSUAL = 'Mensual'
+
     FREQUENCY_CHOICES = [
-        (DAILY, 'Daily'),
-        (WEEKLY, 'Weekly'),
-        (MONTHLY, 'Monthly'),
+        (DIARIO, 'Diario'),
+        (SEMANAL, 'Semanal'),
+        (MENSUAL, 'Mensual'),
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habits')
-    name = models.CharField(max_length=100)
-    frequency = models.CharField(max_length=10, choices=FREQUENCY_CHOICES)
-    category = models.CharField(max_length=50)
-    goal = models.PositiveIntegerField(help_text="Number of repetitions needed to complete the habit.")
+    # Campos del modelo
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='habitos')
+    nombre_habito = models.CharField(max_length=100)
+    frecuencia = models.CharField(max_length=10, choices=FREQUENCY_CHOICES)
+    categoria = models.CharField(max_length=50)
+    objetivo = models.PositiveIntegerField(help_text="Repeticiones necesarias para completar el hábito.")
 
     def __str__(self):
-        return f"{self.name} ({self.user.username})"
+        return f"{self.nombre_habito} ({self.id_usuario.username})"
