763fe487a604b652ef05ffb10c4480c661f3f20e
xelaalex2898 <xelaalex@ciencias.unam.mx>
2024-10-24 22:34:57 -0600

    notifications model changed. one fild added

--- notifications/models.py
+++ notifications/models.py
@@ -7,6 +7,8 @@ class Notification(models.Model):
     message = models.CharField(max_length=255)
     scheduled_time = models.TimeField()
     sent = models.BooleanField(default=False)
+    seen = models.BooleanField(default=False)
+
 
     def __str__(self):
         return f"Notification for {self.habit.name} at {self.scheduled_time}"
