48591f9f159f1d230d7631211c3432979a5d1465
casarin <nsgbanks@ciencias.unam.mx>
2024-10-24 22:27:09 -0600

    progress model added

--- progress/models.py
+++ progress/models.py
@@ -1,14 +1,16 @@
-
 from django.db import models
 from habits.models import Habit
 
-class Progress(models.Model):
-    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='progress')
-    date = models.DateField()
-    completions = models.PositiveIntegerField(default=0)
+class ProgresoHabito(models.Model):
+    id_progreso = models.AutoField(primary_key=True)
+    id_habito = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='progresos')
+    fecha = models.DateField()
+    estado = models.CharField(max_length=50) 
 
     class Meta:
-        unique_together = ('habit', 'date')
+        unique_together = ('id_habito', 'fecha')
+        verbose_name = "Progreso del Hábito"
+        verbose_name_plural = "Progresos de los Hábitos"
 
     def __str__(self):
-        return f"{self.habit.name} - {self.date} - {self.completions}"
+        return f"{self.id_habito.name} - {self.fecha} - {self.estado}"
